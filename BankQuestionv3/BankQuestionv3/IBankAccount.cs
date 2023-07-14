3720a8af2fb49ad338bd3abd19dde778d7c7d4a3
orhanwill <84351507+orhanwill@users.noreply.github.com>
2023-07-14 06:52:19 -0400

    Add files via upload

---
+++ BankQuestionv3/BankQuestionv3/IBankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankQuestionv3
+{
+    
+         public interface IBankAccount
+        {
+            void Deposit(double amount);
+            void Withdraw(double amount);
+        }
+    }
+
---
+++ BankQuestionv3/BankQuestionv3/LoanAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankQuestionv3
+{
+    public class LoanAccount : BankAccount, IBankAccount,ITransaction
+    {
+        public double LoanDuration { get; set; }
+    }
+}
